dca53c2be250d61230691b650b517021bc463ccb
Isichei Phelim <isicheiphelim@gmail.com>
2023-02-25 17:12:01 +0000

    revert code but increased the timeout time

--- js/loader.js
+++ js/loader.js
@@ -1,15 +1,17 @@
-let intervalId = setInterval(progress, 800);
-
 function progress(){
-  var $bar = document.getElementById("bar");
-  var newWidth = parseInt($bar.style.width.replace("%","").replace("px",""));
-  if (newWidth >= 500) {
-    clearInterval(intervalId);
-  } else {
-    $bar.style.width = (newWidth + 60) + "px";
-  }
-  $bar.innerHTML = Math.round(parseInt($bar.style.width.replace("%","").replace("px","")) / 5) + "%";
-  if (Math.round(parseInt($bar.style.width.replace("%","").replace("px","")) / 5) >= 20) {
-    $bar.innerHTML = "Still working ... " + Math.round(parseInt($bar.style.width.replace("%","").replace("px","")) / 5) + "%";
-  }
+  //  console.log('start progress bar')
+    setInterval(function () {
+        var $bar =  document.getElementById("bar");
+    
+        let newWidth=parseInt($bar.style.width.replace("%","").replace("px",""))
+        if (newWidth >= 500) {
+            clearInterval(progress);
+        } else {
+            $bar.style.width=(newWidth + 60)+'px';
+        }
+        $bar.innerHTML=(parseInt($bar.style.width.replace("%","").replace("px","")) / 6 + "%");
+        if (parseInt($bar.style.width.replace("%","").replace("px","")) / 5 == 100){
+          $bar.innerHTML=("Still working ... " + parseInt($bar.style.width.replace("%","").replace("px","")) / 5 + "%");
+        }
+    }, 1000)
 }
