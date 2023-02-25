852dca32824c1f36ab2bd688f53187d746058824
Isichei Phelim <isicheiphelim@gmail.com>
2023-02-25 16:58:34 +0000

    testing progress bar

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,15 @@
+let intervalId = setInterval(progress, 800);
+
 function progress(){
-  //  console.log('start progress bar')
-    setInterval(function () {
-        var $bar =  document.getElementById("bar");
-    
-        let newWidth=parseInt($bar.style.width.replace("%","").replace("px",""))
-        if (newWidth >= 500) {
-            clearInterval(progress);
-        } else {
-            $bar.style.width=(newWidth + 60)+'px';
-        }
-        $bar.innerHTML=(parseInt($bar.style.width.replace("%","").replace("px","")) / 6 + "%");
-        if (parseInt($bar.style.width.replace("%","").replace("px","")) / 5 == 100){
-          $bar.innerHTML=("Still working ... " + parseInt($bar.style.width.replace("%","").replace("px","")) / 5 + "%");
-        }
-    }, 800)
-} 
+  var $bar = document.getElementById("bar");
+  var newWidth = parseInt($bar.style.width.replace("%","").replace("px",""));
+  if (newWidth >= 500) {
+    clearInterval(intervalId);
+  } else {
+    $bar.style.width = (newWidth + 60) + "px";
+  }
+  $bar.innerHTML = (parseInt($bar.style.width.replace("%","").replace("px","")) / 6) + "%";
+  if (parseInt($bar.style.width.replace("%","").replace("px","")) / 5 == 100) {
+    $bar.innerHTML = "Still working ... " + (parseInt($bar.style.width.replace("%","").replace("px","")) / 5) + "%";
+  }
+}
